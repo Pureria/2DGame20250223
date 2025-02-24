f88815f17d356595f9916a3572480f49ea519063
Pureria <117608289+Pureria@users.noreply.github.com>
2025-02-24 17:49:41 +0900

    DynamicBodyの中身実装

--- My2DGame/DynamicBody.cpp
+++ My2DGame/DynamicBody.cpp
@@ -1,1 +1,62 @@
 #include "DynamicBody.h"
+#include "GameObject.h"
+
+DynamicBody::DynamicBody(float gravityScale, GameObject* owner) :
+	_owner(owner),
+	_gravityScale(gravityScale),
+	_velocity(0, 0),
+	_isGravityEnabled(true),
+	_isGrounded(false)
+{
+}
+
+DynamicBody::DynamicBody(float gravityScale, GameObject* owner, bool isGravity) :
+	_owner(owner),
+	_gravityScale(gravityScale),
+	_velocity(0, 0),
+	_isGravityEnabled(isGravity),
+	_isGrounded(false)
+{
+}
+
+DynamicBody::~DynamicBody()
+{
+}
+
+void DynamicBody::DebugDraw()
+{
+}
+
+void DynamicBody::Update()
+{
+	//重力を適用
+	_velocity.y += 9.8f * _gravityScale;
+	//オーナーの位置を更新
+	sf::Vector2f pos = _owner->GetPosition();
+	pos += _velocity;
+	_owner->SetPosition(pos);
+}
+
+void DynamicBody::Release()
+{
+}
+
+void DynamicBody::SetGravityScale(float gravityScale)
+{
+	_gravityScale = gravityScale;
+}
+
+void DynamicBody::SetVelocity(sf::Vector2f velocity)
+{
+	_velocity = velocity;
+}
+
+void DynamicBody::SetIsGrounded(bool isGrounded)
+{
+	_isGrounded = isGrounded;
+}
+
+void DynamicBody::SetIsGravityEnabled(bool isGravity)
+{
+	_isGravityEnabled = isGravity;
+}
--- My2DGame/DynamicBody.h
+++ My2DGame/DynamicBody.h
@@ -1,5 +1,40 @@
 #pragma once
-class DynamicBody
+#include <SFML/System/Vector2.hpp>
+#include <memory>
+
+class GameObject; //ëOï˚êÈåæ
+
+class DynamicBody : public std::enable_shared_from_this<DynamicBody>
 {
+private:
+	GameObject* _owner;
+	float _gravityScale;
+	bool _isGrounded;
+	bool _isGravityEnabled;
+	sf::Vector2f _velocity;
+
+public:
+	DynamicBody(float gravityScale, GameObject* owner);
+	DynamicBody(float gravityScale, GameObject* owner, bool isGravity);
+	~DynamicBody();
+	void DebugDraw();
+	void Update();
+	void Release();
+
+	void SetGravityScale(float gravityScale);
+	void SetVelocity(sf::Vector2f velocity);
+	void SetIsGrounded(bool isGrounded);
+	void SetIsGravityEnabled(bool isGravity);
+
+	bool GetIsGrounded() const;
+	bool GetIsGravityEnabled() const;
+	float GetGravityScale() const;
+	sf::Vector2f GetVelocity() const;
+	GameObject* GetOwner();
 };
 
+inline float DynamicBody::GetGravityScale() const { return _gravityScale; }
+inline sf::Vector2f DynamicBody::GetVelocity() const { return _velocity; }
+inline GameObject* DynamicBody::GetOwner() { return _owner; }
+inline bool DynamicBody::GetIsGrounded() const { return _isGrounded; }
+inline bool DynamicBody::GetIsGravityEnabled() const { return _isGravityEnabled; }
